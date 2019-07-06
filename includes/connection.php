1842775c863923f61d83cf59f96f5f5d5743ad62
bosen li <bosenli2013@hotmail.com>
2019-07-06 14:51:48 -0400

    edited

---
+++ includes/connection.php
@@ -0,0 +1,11 @@
+<?php
+$dbServername = "localhost";
+$dbUseraname = "root";
+$dbPassword = "";
+$dbName = "restaurant_directory"; 
+
+$conn = mysqli_connect($dbServername, $dbUseraname, $dbPassword ,$dbName);
+
+
+
+?>
